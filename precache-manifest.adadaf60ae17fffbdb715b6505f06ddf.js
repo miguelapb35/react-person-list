8d9a7c41ccadb6c08f25dd9c30f42e8b9422d61c
Miguel Angel <miguelapb35@gmail.com>
2020-05-13 18:21:53 -0500

    Updates

---
+++ precache-manifest.adadaf60ae17fffbdb715b6505f06ddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f3558027f8b6d036ff71631ebbb8e0",
+    "url": "/react-person-list/index.html"
+  },
+  {
+    "revision": "6e6cf9d7ca489dfc6a20",
+    "url": "/react-person-list/static/css/main.9f81a166.chunk.css"
+  },
+  {
+    "revision": "582a15bb7f0797c3d9d3",
+    "url": "/react-person-list/static/js/2.650714ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-person-list/static/js/2.650714ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6cf9d7ca489dfc6a20",
+    "url": "/react-person-list/static/js/main.8c9f0d08.chunk.js"
+  },
+  {
+    "revision": "680817c4ecc7c8e50ddb",
+    "url": "/react-person-list/static/js/runtime-main.30786851.js"
+  }
+]);
